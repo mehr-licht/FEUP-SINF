22f8a46ae1523e4d447a0eaf9d286c1a386e506f
duartefaria <duartefariaaa@gmail.com>
2019-12-03 13:44:37 +0000

    Fix some issues

--- machip/src/App.js
+++ machip/src/App.js
@@ -6,13 +6,17 @@ import SalesOrders from "./components/sale_orders/sale_orders";
 function App() {
   //getToken();
   return (
+    <div>
       <Navbar bg="dark" variant="dark">
         <Navbar.Brand href="#home">
           React Bootstrap
         </Navbar.Brand>
       </Navbar>
-    </div>
+      <div>
         <SalesOrders></SalesOrders>
+      </div>
+      
+    </div>
   );
 }
 
