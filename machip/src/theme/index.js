8c10d3a41f09c2082d3e5a7c060cbb11c1684c44
Pedro Gonçalves <pedrokraktuga@gmail.com>
2019-12-09 22:41:28 +0000

    fix: Add theme index

---
+++ machip/src/theme/index.js
@@ -0,0 +1,14 @@
+ 
+import { createMuiTheme } from '@material-ui/core';
+
+import palette from './palette';
+
+const theme = createMuiTheme({
+  palette,
+  zIndex: {
+    appBar: 1200,
+    drawer: 1100
+  }
+});
+
+export default theme;
