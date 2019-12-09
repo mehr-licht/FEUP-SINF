cfc179ea4efc059e14d229b2672a2b028a4196dd
Pedro Gonçalves <pedrokraktuga@gmail.com>
2019-12-09 22:39:47 +0000

    feat: Add theme to app

--- machip/src/App.js
+++ machip/src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { ThemeProvider } from '@material-ui/styles';
 import Layout from './components/utils/Layout';
+import theme from './theme';
 
-function App() {
-  return (
-    <div>
-      <Layout/>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+          <Layout/>
+      </ThemeProvider>
+    );
+  }
 }
-
-export default App;
