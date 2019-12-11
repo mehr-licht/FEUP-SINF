206130174824d01ae2611b35a1100dbbb20c2a49
Pedro Gonçalves <pedrokraktuga@gmail.com>
2019-12-11 10:10:08 +0000

    fix: Small styling fixes

--- machip/src/pages/Home.js
+++ machip/src/pages/Home.js
@@ -3,8 +3,6 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import { navigate } from '@reach/router'
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
@@ -16,18 +14,14 @@ const useStyles = makeStyles(theme => ({
       marginTop: theme.spacing(8),
       display: 'flex',
       flexDirection: 'column',
-      alignItems: 'center',
+      alignItems: 'center'
     },
     avatar: {
       margin: theme.spacing(1),
-      backgroundColor: theme.palette.grey,
-    },
-    form: {
-      width: '100%',
-      marginTop: theme.spacing(1),
+      backgroundColor: theme.palette.grey
     },
     submit: {
-      margin: theme.spacing(3, 0, 2),
+      margin: theme.spacing(3, 0, 2)
     },
 }));
 
@@ -39,13 +33,13 @@ const Home = () => {
             <CssBaseline />
             <div className={classes.paper}>
                 <Avatar className={classes.avatar}>
-                <LockOutlinedIcon />
+                  <LockOutlinedIcon />
                 </Avatar>
                 <Typography component="h1" variant="h5">
                 Sign in
                 </Typography>
                 <TextField
-                    variant="outlined"
+                  variant="outlined"
                     margin="normal"
                     required
                     fullWidth
@@ -56,7 +50,7 @@ const Home = () => {
                     autoFocus
                 />
                 <TextField
-                    variant="outlined"
+                  variant="outlined"
                     margin="normal"
                     required
                     fullWidth
@@ -66,19 +60,15 @@ const Home = () => {
                     id="password"
                     autoComplete="current-password"
                 />
-                <FormControlLabel
-                    control={<Checkbox value="remember" color="primary" />}
-                    label="Remember me"
-                />
                 <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    color="primary"
-                    className={classes.submit}
-                    onClick={ () => {navigate('/dashboard')}}
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  className={classes.submit}
+                  onClick={ () => {navigate('/dashboard')}}
                 >
-                    Sign In
+                  Sign In
                 </Button>
             </div>
         </Container>
