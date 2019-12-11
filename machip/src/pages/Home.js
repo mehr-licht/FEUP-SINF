c055e9ef1da9c01ef58f98efbd80b41f7b66b52f
Christopher Abreu <up201604735@fe.up.pt>
2019-12-11 16:50:15 +0000

    changed colors on home screen

--- machip/src/pages/Home.js
+++ machip/src/pages/Home.js
@@ -4,76 +4,138 @@ import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import { navigate } from '@reach/router'
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import SvgIcon from '@material-ui/core/SvgIcon';
+import MemoryIcon from '@material-ui/icons/Memory';
+import { Card, Hidden } from '@material-ui/core';
+
+
 
 const useStyles = makeStyles(theme => ({
-    paper: {
-      marginTop: theme.spacing(8),
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center'
+  main: {
+    display: 'flex',
+    minHeight: '100vh',
+    width: '100%',
+    backgroundColor: theme.palette.black,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  card: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: '2em',
+    backgroundColor: theme.palette.gray
+  },
+
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.green
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    backgroundColor: theme.palette.green
+  },
+  typography: {
+    color: theme.palette.neon_green
+
+  },
+  textField: {
+    '& .MuiInput-underline:after': {
+      borderBottomColor: theme.palette.neon_green,
     },
-    avatar: {
-      margin: theme.spacing(1),
-      backgroundColor: theme.palette.grey
+    '& .MuiOutlinedInput-root': {
+      '&.Mui-focused fieldset': {
+        borderColor: theme.palette.neon_green,
+      }
+    }
+  },
+  input: {
+    backgroundColor: theme.palette.light_gray,
+    color: theme.palette.black,
+  },
+
+  labelRoot: {
+    color: "black",
+    "&$labelFocused": {
+      fontWeight: 'bold',
+      color: theme.palette.neon_green,
+      position: 'relative',
     },
-    submit: {
-      margin: theme.spacing(3, 0, 2)
-    },
+    
+  },
+  labelFocused: {
+  }
 }));
 
 const Home = () => {
-    const classes = useStyles();
-    return (
-      <>
-        <Container component="main" maxWidth="xs">
-            <CssBaseline />
-            <div className={classes.paper}>
-                <Avatar className={classes.avatar}>
-                  <LockOutlinedIcon />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                Sign in
+  const classes = useStyles();
+  return (
+    <div className={classes.main}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Card className={classes.card}>
+          <Avatar className={classes.avatar}>
+            <SvgIcon component={MemoryIcon} />
+          </Avatar>
+          <Typography className={classes.typography} component="h1" variant="h5">
+            Sign in
                 </Typography>
-                <TextField
-                  variant="outlined"
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    autoComplete="email"
-                    autoFocus
-                />
-                <TextField
-                  variant="outlined"
-                    margin="normal"
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="current-password"
-                />
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  color="primary"
-                  className={classes.submit}
-                  onClick={ () => {navigate('/dashboard')}}
-                >
-                  Sign In
+          <TextField className={classes.textField}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoFocus
+            InputProps={{
+              className: classes.input,
+            }}
+            InputLabelProps={{
+              classes: {
+                root: classes.labelRoot,
+                focused: classes.labelFocused
+              }
+            }}
+          />
+          <TextField className={classes.textField}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            InputProps={{
+              className: classes.input,
+            }}
+            InputLabelProps={{
+              classes: {
+                root: classes.labelRoot,
+                focused: classes.labelFocused
+              }
+            }}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={() => { navigate('/dashboard') }}
+          >
+            Login
                 </Button>
-            </div>
-        </Container>
-      </>
-    );
+        </Card>
+      </Container>
+    </div>
+  );
 }
 
 export default Home;
