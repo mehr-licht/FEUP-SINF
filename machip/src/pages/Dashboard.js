5e1bf707b1fce657104d8eca5f5170893155c5e8
Pedro Gonçalves <pedrokraktuga@gmail.com>
2019-12-11 15:17:30 +0000

    feat: Add dashboard cards

--- machip/src/pages/Dashboard.js
+++ machip/src/pages/Dashboard.js
@@ -1,9 +1,57 @@
 import React from 'react';
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import Grid from "@material-ui/core/Grid";
+
 import Layout from '../components/common/Layout'
 
+const useStyles = makeStyles(theme => ({
+    root: {
+      flexGrow: 1,
+      paddingLeft: '20px',
+      paddingTop: '10px',
+    },
+    card: {
+      padding: theme.spacing(1),
+      textAlign: "center",
+      color: theme.palette.white,
+      backgroundColor: theme.palette.gray,
+      height: '40vh',
+    }
+  }));
+
 const Dashboard = () => {
+    const classes = useStyles();
+
+    function FormRow() {
+        return (
+        <React.Fragment>
+            <Grid item xs>
+                <Card className={classes.card}>
+                    <h2>Sales</h2>
+                </Card>
+            </Grid>
+            <Grid item xs>
+                <Card className={classes.card}>
+                    <h2>Orders</h2>
+                </Card>
+            </Grid>
+        </React.Fragment>
+        );
+    }
+
     return (
         <Layout >
+            <div className={classes.root}>
+                <Grid container spacing={6}>
+                    <Grid container item xs={12} spacing={4} >
+                        <FormRow />
+                    </Grid>
+                    <Grid container item xs={12} spacing={4}>
+                        <FormRow />
+                    </Grid>
+                </Grid>
+            </div>
         </Layout>
     );
 }
